2962e86811d90b67e6f28ad1e24e756a9643cff2
RikDuijm <rikduijm@gmail.com>
2019-11-28 11:07:13 +0000

    correct ALLOWED_HOST

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://riks-first-blog.herokuapp.com/']
+ALLOWED_HOSTS = ['riks-first-blog.herokuapp.com']
 
 
 # Application definition
